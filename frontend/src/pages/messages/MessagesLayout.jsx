886256d356308e3123fbd01893d195425324547e
syedmukheeth <smpeer05@gmail.com>
2026-04-24 21:39:59 +0530

    Cleanup: Purging legacy messaging code for V2.0 Scratch Rebuild.

--- frontend/src/pages/messages/MessagesLayout.jsx
+++
@@ -1,9 +0,0 @@
-export default function MessagesLayout({ children }) {
-    return (
-        <div className="dm-premium-container">
-            <div className="dm-premium-content">
-                {children}
-            </div>
-        </div>
-    )
-}
